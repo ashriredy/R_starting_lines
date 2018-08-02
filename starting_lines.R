8f1f808ddc4f6ecbc8569ed2d08775139eea5619
Ashrith Reddy <5827632+ashriredy@users.noreply.github.com>
2018-08-02 19:07:49 +0530

    Minor edits

--- starting_lines.R
+++ starting_lines.R
@@ -1,13 +1,17 @@
 #### 00 Setting Up ####
-if(!require("data.table")){install.packages("data.table",dependencies = T);library("data.table")}
-if(!require("dplyr")){install.packages("dplyr",dependencies = T);library("dplyr")}
 if(!require("magrittr")){install.packages("magrittr",dependencies = T);library("magrittr")}
+if(!require("dplyr")){install.packages("dplyr",dependencies = T);library("dplyr")}
 if(!require("tidyr")){install.packages("tidyr",dependencies = T);library("tidyr")}
 if(!require("plyr")){install.packages("plyr",dependencies = T);library("plyr")}
 if(!require("lubridate")){install.packages("lubridate",dependencies = T);library("lubridate")}
+
+if(!require("data.table")){install.packages("data.table",dependencies = T);library("data.table")}
 if(!require("microbenchmark")){install.packages("microbenchmark",dependencies = T);library("microbenchmark")}
+
+if(!require("rstudioapi")){install.packages("rstudioapi",dependencies = T);library("rstudioapi")}
 if(!require("xlsx")){install.packages("xlsx",dependencies = T);library("xlsx")}
 if(!require("ggplot2")){install.packages("ggplot2",dependencies = T);library("ggplot2")}
+
 if(!require("factoextra")){install.packages("factoextra",dependencies = T);library("factoextra")}
 if(!require("cluster")){install.packages("cluster",dependencies = T);library("cluster")}
 if(!require("stats")){install.packages("stats",dependencies = T);library("stats")}
@@ -17,7 +21,7 @@ if(!require("clValid")){install.packages("clValid",dependencies = T);library("cl
 if(!require("beepr")){install.packages("beepr",dependencies = T);library("beepr")}
 if(!require("clustMixType")){install.packages("clustMixType",dependencies = T);library("clustMixType")}
 if(!require("dummies")){install.packages("dummies",dependencies = T);library("dummies")}
-if(!require("rstudioapi")){install.packages("rstudioapi",dependencies = T);library("rstudioapi")}
+
 gc()
 setwd("C:/Users/Ashrith Reddy/Desktop/")
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
